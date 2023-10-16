226914588a6a3c1fa900382b242c8d0eca65f0d3
Jihun Park <jihunpark.tech@gmail.com>
2023-10-16 10:21:17 +0900

    init: project

---
+++ src/main/java/com/site/reon/ReonApplication.java
@@ -0,0 +1,13 @@
+package com.site.reon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/site/reon/ReonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.reon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
