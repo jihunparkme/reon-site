62bca50c3771c8dd932eb8ef960572d027b99f04
Jihun Park <jihunpark.tech@gmail.com>
2023-10-22 15:53:25 +0900

    add: JWT config

---
+++ src/main/java/com/site/reon/global/jwt/CorsFilterConfig.java
@@ -0,0 +1,25 @@
+package com.site.reon.global.jwt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsFilterConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // 사용자 자격 증명 지원 여부
+        config.addAllowedOrigin("*"); // CORS 허용 목록
+        config.addAllowedHeader("*"); // CORS 허용 헤더
+        config.addAllowedMethod("*"); // CORS 허용 메소드
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // CORS 구성 매핑 설정
+
+        return new CorsFilter(source);
+    }
+}
---
+++ src/main/java/com/site/reon/global/jwt/JwtSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.site.reon.global.jwt;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private TokenProvider tokenProvider;
+
+    public JwtSecurityConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    /**
+     * JwtFilter 를 Filter 로 등록
+     */
+    @Override
+    public void configure(HttpSecurity http) {
+        http.addFilterBefore(
+                new JwtFilter(tokenProvider),
+                UsernamePasswordAuthenticationFilter.class
+        );
+    }
+}
