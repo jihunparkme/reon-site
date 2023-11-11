178acc75e08787cda8240ff33ffb691cc683bfb9
Jihun Park <jihunpark.tech@gmail.com>
2023-11-11 13:25:29 +0900

    edit: sign up page

--- src/main/resources/static/js/login/login.js
+++ src/main/resources/static/js/login/login.js
@@ -1,8 +1,10 @@
-$('.message a').click(function(){
+$('.message a').click(function () {
     $('.transform-form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    let loginTitle = $(this).text();
+    $("#login-title").text(loginTitle);
 });
 
-function login(){
+function login() {
     let data = {
         "email": $('#login-email').val(),
         "password": $('#login-password').val()
@@ -15,9 +17,10 @@ function login(){
         dataType: 'json',
         data: JSON.stringify(data),
         contentType: 'application/json; charset=utf-8'
-    }).done(function(response) {
+    }).done(function (response) {
         location.href = "/";
     }).fail(function (error) {
         alert('이메일 또는 비밀번호를 확인해 주세요.');
     });
 }
+ㅇ
