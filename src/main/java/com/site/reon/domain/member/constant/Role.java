fed7a953c99f05175b3afa66bd6fb78aec30f87c
Jihun Park <jihunpark.tech@gmail.com>
2023-11-12 15:10:01 +0900

    refactor: edit role type

--- src/main/java/com/site/reon/domain/member/constant/Role.java
+++ src/main/java/com/site/reon/domain/member/constant/Role.java
@@ -5,9 +5,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Role {
 
-    GUEST("ROLE_GUEST", "손님"),
-    USER("ROLE_USER", "일반 사용자"),
-    ADMIN("ROLE_ADMIN", "관리자");
+    GUEST("ROLE_GUEST", "GUEST"),
+    USER("ROLE_USER", "USER"),
+    ADMIN("ROLE_ADMIN", "ADMIN");
 
     private final String key;
     private final String title;
