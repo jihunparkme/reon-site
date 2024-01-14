99a6af5c60833ee639fafdd1b2a892934964632d
Jihun Park <jihunpark.tech@gmail.com>
2024-01-14 20:25:20 +0900

    feat: 소셜 로그인 페이지 추가

---
+++ src/main/resources/static/js/login/email.js
@@ -0,0 +1,47 @@
+$('.message a').click(function () {
+    $('.transform-form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    let loginTitle = $(this).text();
+    $("#login-title").text(loginTitle);
+});
+
+function login() {
+    let data = {
+        "email": $('#login-email').val(),
+        "password": $('#login-password').val()
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: "/member/authenticate",
+        dataType: 'json',
+        data: JSON.stringify(data),
+        contentType: 'application/json; charset=utf-8'
+    }).done(function (response) {
+        location.href = "/";
+    }).fail(function () {
+        alert('이메일 또는 비밀번호를 확인해 주세요.');
+    });
+}
+
+function signUp() {
+    let data = {
+        "firstName": $('#sign-up-first-name').val(),
+        "lastName": $('#sign-up-last-name').val(),
+        "email": $('#sign-up-email').val(),
+        "password": $('#sign-up-password').val()
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: "/member/sign-up",
+        dataType: 'json',
+        data: JSON.stringify(data),
+        contentType: 'application/json; charset=utf-8'
+    }).done(function (response) {
+        alert('회원가입이 완료되었습니다. 다시 로그인해 주세요.');
+        location.href = "/";
+    }).fail(function (error) {
+        let errorMessage = error.responseText;
+        alert(errorMessage);
+    });
+}
