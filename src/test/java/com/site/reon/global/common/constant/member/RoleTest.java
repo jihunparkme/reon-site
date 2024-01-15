84c55a45ac18de0a558b881526bff1aa84801c2a
Jihun Park <jihunpark.tech@gmail.com>
2024-01-15 22:26:10 +0900

    test: role test

--- src/test/java/com/site/reon/global/common/constant/member/RoleTest.java
+++ src/test/java/com/site/reon/global/common/constant/member/RoleTest.java
@@ -3,8 +3,6 @@ package com.site.reon.global.common.constant.member;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class RoleTest {
     @Test
     void method() throws Exception {
