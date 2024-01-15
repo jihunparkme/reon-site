37ffb0823039706c2144bd0f87372db7539f0ffb
Jihun Park <jihunpark.tech@gmail.com>
2024-01-15 21:04:33 +0900

    refactor: service test 분리

---
+++ src/test/java/com/site/reon/aggregate/member/service/MemberLoginServiceImplTest.java
@@ -0,0 +1,57 @@
+package com.site.reon.aggregate.member.service;
+
+import com.site.reon.aggregate.member.service.dto.MemberDto;
+import com.site.reon.aggregate.member.service.dto.SignUpDto;
+import com.site.reon.global.security.exception.DuplicateMemberException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+class MemberLoginServiceImplTest {
+
+    @Autowired
+    private MemberLoginService memberLoginService;
+
+    @Autowired
+    private MemberService memberService;
+
+    @Test
+    void signup_is_success() throws Exception {
+        String email = "aaron@gmail.com";
+        SignUpDto signUp = SignUpDto.builder()
+                .firstName("aaron")
+                .lastName("park")
+                .email(email)
+                .password("aaron")
+                .build();
+
+        memberLoginService.signup(signUp);
+
+        MemberDto member = memberService.getMemberWithAuthorities(email);
+        assertEquals("aaron", member.getFirstName());
+        assertEquals("park", member.getLastName());
+        assertEquals(email, member.getEmail());
+    }
+
+    @Test
+    void signup_is_fail() throws Exception {
+        String email = "admin@gmail.com";
+        SignUpDto signUp = SignUpDto.builder()
+                .firstName("admin")
+                .lastName("park")
+                .email(email)
+                .password("admin")
+                .build();
+
+        DuplicateMemberException exception = assertThrows(DuplicateMemberException.class, () ->
+                memberLoginService.signup(signUp)
+        );
+
+        assertEquals("이미 가입되어 있는 이메일입니다.", exception.getMessage());
+    }
+}
