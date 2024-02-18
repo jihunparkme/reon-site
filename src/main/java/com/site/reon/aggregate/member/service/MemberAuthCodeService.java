043c3fffce3c43dde90d6c19a2eea5dd05ba43ab
Jihun Park <jihunpark.tech@gmail.com>
2024-02-18 21:42:43 +0900

    refactor: add redis key prefix

--- src/main/java/com/site/reon/aggregate/member/service/MemberAuthCodeService.java
+++ src/main/java/com/site/reon/aggregate/member/service/MemberAuthCodeService.java
@@ -1,6 +1,7 @@
 package com.site.reon.aggregate.member.service;
 
 import com.site.reon.global.common.constant.mail.MailSubject;
+import com.site.reon.global.common.constant.redis.KeyPrefix;
 import com.site.reon.global.common.event.Events;
 import com.site.reon.global.common.util.AuthCodeUtil;
 import com.site.reon.global.common.util.infra.RedisUtilService;
@@ -19,10 +20,10 @@ import java.util.Optional;
 public class MemberAuthCodeService {
     private final RedisUtilService redisUtilService;
 
-    public void sendAuthCode(String purpose, String email) {
+    public void sendAuthenticationCodeByEmail(String purpose, String email) {
         log.info("[Event] send Auth Code Mail Event. purpose: {}, email: {}", purpose, email);
         String authCode = AuthCodeUtil.generateAuthCodeString();
-        redisUtilService.setValueExpire(email, authCode, 180L);
+        redisUtilService.setValueExpire(KeyPrefix.SIGN_UP.prefix() + email, authCode, 180L);
 
         Events.raise(SendMailEvent.builder()
                 .subject(MailSubject.AUTH_CODE.title())
