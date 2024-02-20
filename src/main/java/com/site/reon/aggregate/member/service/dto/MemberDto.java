39b9a4237fed7d6d663b7731f120c69fb157201a
Jihun Park <jihunpark.tech@gmail.com>
2024-02-20 21:45:28 +0900

    add: validation message

--- src/main/java/com/site/reon/aggregate/member/service/dto/MemberDto.java
+++ src/main/java/com/site/reon/aggregate/member/service/dto/MemberDto.java
@@ -24,32 +24,32 @@ public class MemberDto {
     private MemberType type;
 
     @NotNull
-    @Size(min = 1, max = 30)
+    @Size(min = 1, max = 30, message = "The first name length must be between 1 and 30 characters.")
     private String firstName;
 
     @NotNull
-    @Size(min = 1, max = 30)
+    @Size(min = 1, max = 30, message = "The last name length must be between 1 and 30 characters.")
     private String lastName;
 
     @NotNull
-    @Size(min = 1, max = 50)
+    @Size(min = 1, max = 50, message = "The email length must be between 1 and 50 characters.")
     private String email;
 
     @NotNull
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @Size(min = 3, max = 100)
+    @Size(min = 3, max = 100, message = "The password length must be between 3 and 100 characters.")
     private String password;
 
     @NotNull
-    @Size(min = 3, max = 20)
+    @Size(min = 3, max = 20, message = "The phone number length must be between 3 and 20 characters.")
     private String phone;
 
     @NotNull
-    @Size(min = 3, max = 100)
+    @Size(min = 3, max = 100, message = "The product code length must be between 3 and 100 characters.")
     private String prdCode;
 
     @NotNull
-    @Size(min = 3, max = 100)
+    @Size(min = 3, max = 100, message = "The roaster S/N length must be between 3 and 100 characters.")
     private String roasterSn;
 
     private boolean activated;
@@ -57,10 +57,10 @@ public class MemberDto {
     private Set<AuthorityDto> authorityDtoSet;
 
     // COMPANY
-    @Size(min = 3, max = 30)
+    @Size(min = 3, max = 30, message = "The company name length must be between 3 and 30 characters.")
     private String companyName;
 
-    @Size(min = 3, max = 1000)
+    @Size(min = 3, max = 1000, message = "The address length must be between 3 and 1000 characters.")
     private String address;
 
     private String picture;
--- src/main/java/com/site/reon/aggregate/member/service/dto/MemberEditRequest.java
+++ src/main/java/com/site/reon/aggregate/member/service/dto/MemberEditRequest.java
@@ -19,24 +19,24 @@ public class MemberEditRequest {
     private MemberType type;
 
     @NotNull(message = "firstName is required.")
-    @Size(min = 1, max = 30, message = "firstName size must be between 1 and 30.")
+    @Size(min = 1, max = 30, message = "The first name length must be between 1 and 30 characters.")
     private String firstName;
 
-    @Size(max = 30)
+    @Size(max = 30, message = "The last name length must not exceed 30 characters.")
     private String lastName;
 
-    @Size(max = 20)
+    @Size(max = 20, message = "The phone number length must not exceed 20 characters.")
     private String phone;
 
-    @Size(max = 100)
+    @Size(max = 100, message = "The product code length must not exceed 100 characters.")
     private String prdCode;
 
-    @Size(max = 100)
+    @Size(max = 100, message = "The roaster S/N length must not exceed 100 characters.")
     private String roasterSn;
 
-    @Size(max = 30)
+    @Size(max = 30, message = "The company name length must not exceed 30 characters.")
     private String companyName;
 
-    @Size(max = 1000)
+    @Size(max = 1000, message = "The address length must not exceed 1000 characters.")
     private String address;
 }
