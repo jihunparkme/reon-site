bc0c292752f940bf00293f4e15612c7531ea4e0e
Jihun Park <jihunpark.tech@gmail.com>
2024-02-24 15:10:11 +0900

    feat: add field in RoastingRecordResponse

--- src/main/java/com/site/reon/aggregate/record/service/dto/RoastingRecordResponse.java
+++ src/main/java/com/site/reon/aggregate/record/service/dto/RoastingRecordResponse.java
@@ -30,6 +30,15 @@ public class RoastingRecordResponse {
     private LocalDateTime createdDt;
     private LocalDateTime modifiedDt;
 
+    private String crackPoint; // 크랙 포인트(1차, 2차). [30.3, 50.3]
+    private String crackPointTime; // 크랙 시간(1차, 2차). [2024-02-20 15:00:15 +0000, 2024-02-20 15:00:45 +0000]
+    private String turningPointTemp; // 터닝 포인트 온도. [30.3]
+    private String turningPointTime; // 터닝 포인트 시간. [2024-02-20 15:00:18 +0000]
+    private float preheatTemp; // 예열 온도. 100.3
+    private String disposeTemp; // 배출 온도. [95.3]
+    private String disposeTime; //배출 시간. [2024-02-20 15:00:18 +0000]
+    private int inputCapacity; // 용량(g). 40
+
     public static RoastingRecordResponse of(RoastingRecord roastingRecord) {
         return RoastingRecordResponse.builder()
                 .id(roastingRecord.getId())
@@ -43,6 +52,14 @@ public class RoastingRecordResponse {
                 .ror(roastingRecord.getRor())
                 .roasterSn(roastingRecord.getRoasterSn())
                 .memberId(roastingRecord.getMemberId())
+                .crackPoint(roastingRecord.getCrackPoint())
+                .crackPointTime(roastingRecord.getCrackPointTime())
+                .turningPointTemp(roastingRecord.getTurningPointTemp())
+                .turningPointTime(roastingRecord.getTurningPointTime())
+                .preheatTemp(roastingRecord.getPreheatTemp())
+                .disposeTemp(roastingRecord.getDisposeTemp())
+                .disposeTime(roastingRecord.getDisposeTime())
+                .inputCapacity(roastingRecord.getInputCapacity())
                 .createdDt(roastingRecord.getCreatedDt())
                 .modifiedDt(roastingRecord.getModifiedDt())
                 .build();
