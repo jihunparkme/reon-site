a1135d8c5fd00897c9ef915f6853a3afee16f857
Jihun Park <jihunpark.tech@gmail.com>
2024-03-03 18:48:44 +0900

    feat: apple login authentication request

---
+++ src/main/java/com/site/reon/aggregate/member/controller/MemberAppleOauth2LoginController.java
@@ -0,0 +1,31 @@
+package com.site.reon.aggregate.member.controller;
+
+import com.site.reon.global.common.property.AppleOauth2Property;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Slf4j
+@Controller
+@RequestMapping("/login/oauth2")
+@RequiredArgsConstructor
+public class MemberAppleOauth2LoginController {
+
+    private final AppleOauth2Property appleOauth2Property;
+
+    @GetMapping(value = "/authorization/apple")
+    public String appleLogin(RedirectAttributes attr) {
+
+        attr.addAttribute("client_id", appleOauth2Property.getClientId());
+        attr.addAttribute("redirect_uri", appleOauth2Property.getRedirectUri());
+        attr.addAttribute("nonce", appleOauth2Property.getNonce());
+        attr.addAttribute("response_type", appleOauth2Property.getResponseType());
+        attr.addAttribute("scope", appleOauth2Property.getScope());
+        attr.addAttribute("response_mode", appleOauth2Property.getResponseMode());
+
+        return "redirect:" + appleOauth2Property.getAuthorizationUri();
+    }
+}
