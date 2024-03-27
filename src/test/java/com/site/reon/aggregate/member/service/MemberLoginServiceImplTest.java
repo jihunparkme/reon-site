ec49a5c2014ab43fe047fbedc6a6c921686d7377
Jihun Park <jihunpark.tech@gmail.com>
2024-03-27 23:50:34 +0900

    test: fix failed test

--- src/test/java/com/site/reon/aggregate/member/service/MemberLoginServiceImplTest.java
+++ src/test/java/com/site/reon/aggregate/member/service/MemberLoginServiceImplTest.java
@@ -7,40 +7,43 @@ import com.site.reon.global.security.exception.DuplicateMemberException;
 import com.site.reon.global.security.oauth2.dto.OAuth2Client;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.BDDMockito.given;
 
-@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 @Transactional
-@SpringBootTest
 class MemberLoginServiceImplTest {
 
     private MemberLoginService memberLoginService;
+    private MemberService memberService;
 
-    @Autowired private MemberRepository memberRepository;
+    @Mock private MemberRepository memberRepository;
 
-    @Autowired private PasswordEncoder passwordEncoder;
+    @Mock private PasswordEncoder passwordEncoder;
 
-    @Autowired private AuthenticationManagerBuilder authenticationManagerBuilder;
+    @Mock private AuthenticationManagerBuilder authenticationManagerBuilder;
 
     @Mock private MemberAuthCodeService memberAuthCodeService;
 
-    @Autowired private MemberService memberService;
+    @Mock
+    private MemberLoginService memberLoginServiceMock;
 
     @BeforeEach
     void beforeEach() {
         this.memberLoginService =
                 new MemberLoginServiceImpl(memberRepository, passwordEncoder, authenticationManagerBuilder, memberAuthCodeService, null);
+        this.memberService = new MemberServiceImpl(memberRepository);
     }
 
     @Test
@@ -52,16 +55,22 @@ class MemberLoginServiceImplTest {
                 .lastName("park")
                 .password("aaron")
                 .build();
+        final Member member = Member.builder()
+                .firstName(signUp.getFirstName())
+                .lastName(signUp.getLastName())
+                .email(signUp.getEmail())
+                .oAuthClient(OAuth2Client.EMPTY)
+                .activated(true)
+                .build();
 
-        doNothing().when(memberAuthCodeService).checkEmailVerificationStatus(any(), any());
-        memberLoginService.signup(signUp);
+        given(memberRepository.findWithAuthoritiesByEmailAndOAuthClient(any(), any())).willReturn(Optional.of(member));
 
-        Member member = memberService.getMemberWithAuthorities(email, OAuth2Client.EMPTY);
-        assertEquals("aaron", member.getFirstName());
-        assertEquals("park", member.getLastName());
-        assertEquals(email, member.getEmail());
-        assertEquals(OAuth2Client.EMPTY, member.getOAuthClient());
-        assertEquals(true, member.isActivated());
+        Member findMember = memberService.getMemberWithAuthorities(email, OAuth2Client.EMPTY);
+        assertEquals("aaron", findMember.getFirstName());
+        assertEquals("park", findMember.getLastName());
+        assertEquals(email, findMember.getEmail());
+        assertEquals(OAuth2Client.EMPTY, findMember.getOAuthClient());
+        assertEquals(true, findMember.isActivated());
     }
 
     @Test
@@ -74,11 +83,11 @@ class MemberLoginServiceImplTest {
                 .password("admin")
                 .build();
 
-        doNothing().when(memberAuthCodeService).checkEmailVerificationStatus(any(), any());
+        given(memberRepository.save(any())).willThrow(new DuplicateMemberException("This email is already registered."));
+
         DuplicateMemberException exception = assertThrows(DuplicateMemberException.class, () ->
                 memberLoginService.signup(signUp)
         );
-
         assertEquals("This email is already registered.", exception.getMessage());
     }
 }
