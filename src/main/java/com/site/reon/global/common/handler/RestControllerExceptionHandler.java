08e96d3f0835c11d0bbf267b138787b366d2aa78
Jihun Park <jihunpark.tech@gmail.com>
2024-04-09 17:36:57 +0900

    refactor: RestControllerExceptionHandler

--- src/main/java/com/site/reon/global/common/handler/RestControllerExceptionHandler.java
+++ src/main/java/com/site/reon/global/common/handler/RestControllerExceptionHandler.java
@@ -1,23 +1,49 @@
 package com.site.reon.global.common.handler;
 
 import com.site.reon.global.common.dto.BasicResponse;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.BeanCreationNotAllowedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.net.BindException;
 
 @Slf4j
 @RestControllerAdvice
 public class RestControllerExceptionHandler {
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleCustomException(IllegalArgumentException ex) {
+    @ExceptionHandler({
+            MethodArgumentTypeMismatchException.class
+            MethodArgumentNotValidException.class,
+            BindException.class,
+            ConstraintViolationException.class,
+            IllegalArgumentException.class
+    })
+    public ResponseEntity handleBadRequest(IllegalArgumentException ex) {
         return BasicResponse.clientError(ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex) {
-        log.error("RoastingRecordApiController.RoastingRecords Exception: ", ex);
+    public ResponseEntity handleException(Exception ex) {
+        log.error("Exception. ", ex);
         return BasicResponse.internalServerError(ex.getMessage());
+    }
+
+    @ExceptionHandler(BeanCreationNotAllowedException.class)
+    public ResponseEntity handleBeanCreationNotAllowedException(Exception ex) {
+        log.error("BeanCreationNotAllowedException. ", ex);
+        return new ResponseEntity("BeanCreationNotAllowedException", HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity handleNoHandlerFoundException(Exception ex) {
+        log.error("NoHandlerFoundException. ", ex);
+        return new ResponseEntity("NoHandlerFoundException", HttpStatus.NOT_FOUND);
     }
 }
