c3b5ecbf34c839b47c216ed81a9efdf398cf6fd5
Jihun Park <jihunpark.tech@gmail.com>
2024-04-10 17:31:15 +0900

    refactor: controller to restController

--- src/main/java/com/site/reon/aggregate/member/controller/GoogleOauth2LoginController.java
+++ src/main/java/com/site/reon/aggregate/member/controller/GoogleOauth2LoginController.java
@@ -7,15 +7,15 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static com.site.reon.global.common.constant.Result.SUCCESS;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/login/oauth2")
 @RequiredArgsConstructor
 public class GoogleOauth2LoginController {
--- src/main/java/com/site/reon/aggregate/member/controller/MemberEmailLoginController.java
+++ src/main/java/com/site/reon/aggregate/member/controller/MemberEmailLoginController.java
@@ -17,15 +17,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.session.FindByIndexNameSessionRepository;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static com.site.reon.global.common.constant.Result.SUCCESS;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/login/email")
 @RequiredArgsConstructor
 public class MemberEmailLoginController {
