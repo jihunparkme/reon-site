7b29970ae6c6f7f8e49529fd9b3d598ebeba4fd2
Jihun Park <jihunpark.tech@gmail.com>
2024-04-15 14:58:36 +0900

    test: add header location

--- src/test/java/com/site/reon/aggregate/member/controller/AppleOAuth2LoginControllerApiTest.java
+++ src/test/java/com/site/reon/aggregate/member/controller/AppleOAuth2LoginControllerApiTest.java
@@ -1,10 +1,12 @@
 package com.site.reon.aggregate.member.controller;
 
 import com.site.reon.global.ApiTest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ActiveProfiles("dev-oauth")
 class AppleOAuth2LoginControllerApiTest extends ApiTest {
@@ -13,20 +15,22 @@ class AppleOAuth2LoginControllerApiTest extends ApiTest {
     void when_authorization_then_redirect_authorization_uri() {
         final var response = AppleOAuth2LoginApiSteps.requestAppleOauth2Authorization();
 
-        Assertions.assertEquals(HttpStatus.OK.value(), response.statusCode());
+        assertEquals(HttpStatus.OK.value(), response.statusCode());
     }
     
     @Test 
     void when_authorizationRedirect_then_redirect_main() {
         final var response = AppleOAuth2LoginApiSteps.requestAuthorizationRedirect();
 
-        Assertions.assertEquals(HttpStatus.FOUND.value(), response.statusCode());
+        assertEquals(HttpStatus.FOUND.value(), response.statusCode());
+        assertTrue(response.header("Location").contains("/"));
     }
 
     @Test
     void when_authorizationRedirect_then_redirect_login_fail_page() {
         final var response = AppleOAuth2LoginApiSteps.requestAuthorizationRedirectEmptyParam();
 
-        Assertions.assertEquals(HttpStatus.FOUND.value(), response.statusCode());
+        assertEquals(HttpStatus.FOUND.value(), response.statusCode());
+        assertTrue(response.header("Location").contains("/login/oauth2/fail"));
     }
 }
