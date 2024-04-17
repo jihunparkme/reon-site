63895db119ae97a3e674085a9f3f776c8ff8ec8e
Jihun Park <jihunpark.tech@gmail.com>
2024-04-17 16:27:26 +0900

    refactor: edit request dto message

--- src/main/java/com/site/reon/aggregate/index/dto/ContactMailRequest.java
+++ src/main/java/com/site/reon/aggregate/index/dto/ContactMailRequest.java
@@ -11,17 +11,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ContactMailRequest {
-    @NotBlank(message = "name is required.")
+    @NotBlank(message = "Name is required.")
     private String name;
 
-    @NotBlank(message = "email is required.")
+    @NotBlank(message = "Email is required.")
     @Pattern(regexp = "^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}", message = "Please check the email format.")
     @Size(min = 3, max = 50, message = "The email length must be between 3 and 50 characters.")
     private String email;
 
-    @NotBlank(message = "subject is required.")
+    @NotBlank(message = "Subject is required.")
     private String subject;
 
-    @NotBlank(message = "message is required.")
+    @NotBlank(message = "Message is required.")
     private String message;
 }
