f54173b34c5afe7e446a8635aee9cc7c5a2fe137
Jihun Park <jihunpark.tech@gmail.com>
2024-05-30 18:16:05 +0900

    feat: apply filter to admin records page

--- src/main/java/com/site/reon/aggregate/record/controller/RoastingRecordAdminPageController.java
+++ src/main/java/com/site/reon/aggregate/record/controller/RoastingRecordAdminPageController.java
@@ -1,5 +1,6 @@
 package com.site.reon.aggregate.record.controller;
 
+import com.site.reon.aggregate.record.query.dto.AdminRecordSearchRequestParam;
 import com.site.reon.aggregate.record.query.dto.RoastingRecordResponse;
 import com.site.reon.aggregate.record.query.service.RoastingRecordFindService;
 import lombok.RequiredArgsConstructor;
@@ -8,9 +9,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
@@ -19,27 +20,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 public class RoastingRecordAdminPageController {
 
-    private final RoastingRecordFindService recordService;
+    private final RoastingRecordFindService recordFindService;
 
     @GetMapping
-    public String list(
-            @RequestParam(value = "page", required = false, defaultValue = "0") final int page,
-            @RequestParam(value = "size", required = false, defaultValue = "10") final int size,
-            Model model) {
-        // TODO: 날짜, 제목, 시리얼넘버 검색
-        final var roastingRecordListPage = recordService.findAllOrderByIdDescPaging(page, size);
+    public String list(@ModelAttribute AdminRecordSearchRequestParam param,
+                       Model model) {
+        final var roastingRecordListPage = recordFindService.findAllOrderByIdDescPaging(param.getPage(), param.getSize());
+        // TODO: call findAllByAdminFilter
+//        final var roastingRecordListPage = recordFindService.findAllByAdminFilter(param);
 
         model.addAttribute("roastingRecordListPage", roastingRecordListPage);
-        model.addAttribute("page", page);
+        model.addAttribute("page", param.getPage());
+
         return "admin/records/record-list";
     }
 
     @GetMapping("{id}")
     public String view(@PathVariable(name = "id") final Long id,
                        Model model) {
-        final RoastingRecordResponse roastingRecord = recordService.findRoastingRecordBy(id);
+        final RoastingRecordResponse roastingRecord = recordFindService.findRoastingRecordBy(id);
 
         model.addAttribute("record", roastingRecord);
+
         return "admin/records/record-view";
     }
 }
